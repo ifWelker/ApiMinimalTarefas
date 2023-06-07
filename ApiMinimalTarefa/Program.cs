924820b4d571e0bf37599af611d1a581202f3f26
ifWelker <fabiowelker@outlook.com>
2023-06-06 23:00:53 -0300

    Criando mapeamento get/id e get/concluida

--- ApiMinimalTarefa/Program.cs
+++ ApiMinimalTarefa/Program.cs
@@ -27,12 +27,20 @@ await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/
 
 app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
 
+app.MapGet("/tarefas/{id}", async(int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+app.MapGet("/tarefas/concluida", async (AppDbContext db) => 
+                            await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
+
 app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa.Id);
 });
+
+
 
 
 app.Run();
@@ -43,7 +51,6 @@ class Tarefa
     public int Id { get; set; }
     public string? Nome { get; set; }
     public bool IsConcluida { get; set; }
-
 
 }
 
